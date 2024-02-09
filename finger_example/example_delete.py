4f034f86ffd659ed0995d0f8c24d6259bc1cc06f
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-02-09 22:50:46 +0900

    -

--- finger_example/example_delete.py
+++ finger_example/example_delete.py
@@ -16,7 +16,8 @@ from pyfingerprint.pyfingerprint import PyFingerprint
 
 ## Tries to initialize the sensor
 try:
-    f = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
+    # f = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
+    f = PyFingerprint('/dev/serial0', 57600, 0xFFFFFFFF, 0x00000000)
 
     if ( f.verifyPassword() == False ):
         raise ValueError('The given fingerprint sensor password is wrong!')
