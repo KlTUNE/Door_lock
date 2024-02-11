9475d0c05531f0925134d7986ed71fae46a6d259
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-02-12 00:21:58 +0900

    -

--- test.py
+++ test.py
@@ -9,4 +9,6 @@ GPIO.setup(Senser, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
     if GPIO.input(Senser) == 1:
         fp_ctl.search()
+        time.sleep(5)
     # time.sleep(1)
+    print(GPIO.input(Senser))
