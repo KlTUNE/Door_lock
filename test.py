b672a0ad0f555be8c756fba2a51fbf884708493f
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-02-11 23:10:57 +0900

    -

--- test.py
+++ test.py
@@ -9,7 +9,9 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(Senser, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    if GPIO.input(Senser) :
-        print(1)
-    else:
+    if GPIO.input(Senser) == 0:
         print(0)
+    else:
+        print(1)
+    time.sleep(1)
+    print("-----------------")
