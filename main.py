bf1ea204b298bf0afbe5442ca717589cd670af80
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-02-12 11:50:02 +0900

    -

---
+++ main.py
@@ -0,0 +1,5 @@
+from modules import fp_ctl, lock_ctl
+import os, time
+
+if __name__ == "__main__":
+    pass
--- old/lock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/pi/lock
-sudo tmux new -s lock -d 'sudo python lock.py'
-sudo tmux new -s listen -d 'sudo node listen.js'
