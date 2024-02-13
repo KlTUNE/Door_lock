9ba5cd8e9347ac09fb6b7f1e3d065e00cee7a962
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-02-14 00:31:37 +0900

    -

--- web_server.py
+++ web_server.py
@@ -30,4 +30,8 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    try:
+        app.run(debug=True, host='0.0.0.0', port=80)
+    except:
+        print("clean")
+        lock_ctl.clean()
