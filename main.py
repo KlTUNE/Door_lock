e1ecfb1e11106bba76003c1cbd9007054f1205cc
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-02-15 22:27:44 +0900

    -

--- main.py
+++ main.py
@@ -11,22 +11,23 @@ GPIO.setup(CLOSE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(TOUCH_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def main():
+    lock_ctl.open()
     while True:
-        if GPIO.input(TOUCH_SENSOR_PIN) == 1:
-            result = fp_ctl.search()
-            if result: lock_ctl.open()
-            else: lock_ctl.lock()
+        try:
+            if GPIO.input(TOUCH_SENSOR_PIN) == 1:
+                result = fp_ctl.search()
+                if result: lock_ctl.open()
+                else: lock_ctl.lock()
 
-        if GPIO.input(OPEN_PIN) == 0:
-            lock_ctl.open()
+            if GPIO.input(OPEN_PIN) == 0:
+                lock_ctl.open()
 
-        if GPIO.input(CLOSE_PIN) == 0:
-            lock_ctl.lock()
+            if GPIO.input(CLOSE_PIN) == 0:
+                lock_ctl.lock()
+
+        except KeyboardInterrupt:
+            print("clean")
+            GPIO.cleanup()
 
 if __name__ == "__main__":
-    try:
-        lock_ctl.open()
-        main()
-    except KeyboardInterrupt:
-        print("clean")
-        GPIO.cleanup()
+    main()
