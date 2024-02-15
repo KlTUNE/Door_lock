119b5af78044ef1ce70921b1b9196f8d3a262efc
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-02-15 22:48:44 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,10 @@ import time
 
 STATUS_LED_PIN = 17
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(STATUS_LED_PIN, GPIO.OUT)
+try:
+    GPIO.setup(STATUS_LED_PIN, GPIO.OUT)
+except:
+    GPIO.cleanup()
+    GPIO.setup(STATUS_LED_PIN, GPIO.OUT)
 
 GPIO.output(STATUS_LED_PIN, 1)
-while True:
-    print("wait")
-    time.sleep(1)
