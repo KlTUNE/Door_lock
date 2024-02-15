8b21ed5057e0423327446932648fcd18f2feb469
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-02-15 23:05:56 +0900

    -

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from modules import fp_ctl, lock_ctl
 import RPi.GPIO as GPIO
+import time
 
 OPEN_PIN = 2
 CLOSE_PIN = 3
 TOUCH_SENSOR_PIN = 4
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(OPEN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(CLOSE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(TOUCH_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -27,8 +29,11 @@ def main():
 
         except KeyboardInterrupt:
             print("cleanup...")
-            lock_ctl.cleanup()
+            # lock_ctl.cleanup()
             GPIO.cleanup()
+            exit(1)
+
+        time.sleep(0.5)
 
 if __name__ == "__main__":
     main()
--- modules/lock_ctl.py
+++ modules/lock_ctl.py
@@ -4,18 +4,21 @@ import time
 SERVO_PIN = 18
 STATUS_LED_PIN = 17
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(SERVO_PIN, GPIO.OUT)
 GPIO.setup(STATUS_LED_PIN, GPIO.OUT)
 servo = GPIO.PWM(SERVO_PIN, 50)
 servo.start(0)
 
 def open():
+    print('open')
     GPIO.output(STATUS_LED_PIN, 0)
     servo.ChangeDutyCycle(7.25)
     time.sleep(0.5)
     servo.ChangeDutyCycle(0)
 
 def lock():
+    print('lock')
     GPIO.output(STATUS_LED_PIN, 1)
     servo.ChangeDutyCycle(2.5)
     time.sleep(0.5)
