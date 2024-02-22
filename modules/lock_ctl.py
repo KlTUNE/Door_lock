8ae708e646f6d88cafa13c07ff971fed313e2755
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-02-22 11:57:57 +0900

    Update lock_ctl.py

--- modules/lock_ctl.py
+++ modules/lock_ctl.py
@@ -12,27 +12,36 @@ GPIO.setup(SERVO_PIN, GPIO.OUT)
 GPIO.setup(STATUS_LED_PIN, GPIO.OUT)
 # サーボのPWMの設定
 servo = GPIO.PWM(SERVO_PIN, 50)
-servo.start(0)
+
+#角度からデューティ比を求め、サーボを動かす
+def servo_angle(angle):
+    #角度からデューティ比を求める
+    duty = 2.5 + (12.0 - 2.5) * (angle + 90) / 180
+    # duty = 3 + (19 / 36) * angle
+    servo.start(0)
+    servo.ChangeDutyCycle(duty)
+    time.sleep(0.3)
+    servo.stop()
 
 # 開錠
 def open():
     print("OPEN")
     # ステータスLEDを消灯
     GPIO.output(STATUS_LED_PIN, 0)
-    # サーボを左に90度回転
-    servo.ChangeDutyCycle(7.25)
-    time.sleep(0.5)
-    servo.ChangeDutyCycle(0)
+    servo_angle(90)
+    # servo.ChangeDutyCycle(7.25)
+    # time.sleep(0.5)
+    # servo.ChangeDutyCycle(0)
 
 # 施錠
 def lock():
     print("LOCK")
     # ステータスLEDを点灯
     GPIO.output(STATUS_LED_PIN, 1)
-    # サーボを右に90度回転
-    servo.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
-    servo.ChangeDutyCycle(0)
+    servo_angle(0)
+    # servo.ChangeDutyCycle(2.5)
+    # time.sleep(0.5)
+    # servo.ChangeDutyCycle(0)
 
 # エラー時のLED点滅
 def error_led():
@@ -45,7 +54,6 @@ def error_led():
 
 # GPIOの初期化
 def cleanup():
-    servo.stop()
     GPIO.cleanup()
 
 if __name__ == "__main__":
